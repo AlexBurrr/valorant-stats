7a046bd901d54d7af480ca4523c2a5f3cc3bee43
Alex <alexandroberumen@gmail.com>
2022-03-26 16:30:01 -0700

    working on api endpoint data

---
+++ src/components/test.js
@@ -0,0 +1,20 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
+
+const Test = () => {
+
+
+useEffect(()=>{
+    axios(
+        'https://api.henrikdev.xyz/valorant/v1/leaderboard/na?limit=10'
+        ).then(res=>console.log(res.data.slice(0,10)))
+},[])
+
+
+  return (
+    <div>test</div>
+  )
+}
+
+export default Test
